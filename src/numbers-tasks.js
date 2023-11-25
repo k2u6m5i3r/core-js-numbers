a609df67d4d276416f213a2a243c86d87f6f2cb9
Sergei Shevchenko <wunder49@yandex.by>
2023-11-25 22:07:51 +0300

    solution for toPrecision

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -424,8 +424,9 @@ function toFixed(number, fractionDigits) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  // throw new Error('Not implemented');
+  return number.toPrecision(precision);
 }
 
 /**
