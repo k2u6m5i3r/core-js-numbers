7a62a513ffd7bf2cdaa1a3d505c0d6badf2b5de3
Sergei Shevchenko <wunder49@yandex.by>
2023-11-25 22:05:10 +0300

    solution for toExponential

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -391,8 +391,9 @@ function numberToStringInBase(number, base) {
  * @example:
  * 12345, 2    => '1.23e+4'
  */
-function toExponential(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toExponential(number, fractionDigits) {
+  // throw new Error('Not implemented');
+  return number.toExponential(fractionDigits);
 }
 
 /**
