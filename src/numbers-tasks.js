9162a8c99bdb8893d5f9e6ffb3744a69d73e5090
Sergei Shevchenko <wunder49@yandex.by>
2023-11-25 22:16:47 +0300

    solution for isSafeInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -525,8 +525,9 @@ function getIntegerOnString(str, base) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  // throw new Error('Not implemented');
+  return Number.isSafeInteger(number);
 }
 
 /**
